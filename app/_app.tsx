644c0345c7a93edf6e81df7599dddb2407fde410
spectredev00 <spectredev00@gmail.com>
2024-07-19 16:38:10 -0400

    app txs

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,7 +1,8 @@
-import './globals.css';
+import { AppProps } from 'next/app';
 import Layout from './layout'; // Assurez-vous d'importer le composant Layout
+import './globals.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
