83d15496675d04503f00e48e82d2776638e619cf
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2024-10-05 13:32:41 -0400

    link fixed

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,15 @@
 import { useLanguage } from "@/context";
+import Link from "next/link";
 export default function Footer() {
-  const { language } = useLanguage(); // Utiliser le contexte de langue
+  const { language } = useLanguage(); 
 
   return (
     <footer id="contact" className="footer">
       <div className="footer__wrapper">
-        <a href="/" className="footer__link">
+        <Link href="/" className="footer__link">
           Mélo Morin
-        </a>
-        <a href="/contact" className="footer__link">
-          {/* Texte dynamique en fonction de la langue */}
+        </Link>
+        <Link href="/contact" className="footer__link">
           <span>{language === "fr" ? "Contact" : "Contact us"}</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -20,7 +20,7 @@ export default function Footer() {
           >
             <path d="M780-400q-27.92 0-49.46-17t-27.92-43H100v-40h602.62q6.38-26 27.92-43 21.54-17 49.46-17 33.6 0 56.8 23.19 23.2 23.18 23.2 56.77 0 33.58-23.2 56.81Q813.6-400 780-400Z" />
           </svg>
-        </a>
+        </Link>
       </div>
     </footer>
   );
